75d969ea8f104f21a722e7ff6d1a178180436567
Fahad Shahbaz <fahadshahbaz166@gmail.com>
2024-08-15 00:13:04 +0500

    Regular code tweaks

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,32 +1,3 @@
-// import React from 'react';
-// import SearchBar from './SearchBar.jsx';
-
-// const Hero = ({ searchQuery, setSearchQuery }) => {
-//   return (
-//     <div className="w-full mx-auto text-center flex justify-center items-center flex-col py-28 lg:py-32 font-montserrat">
-//       <div className="w-[93%] sm:w-[50%] lg:w-[43%] mx-auto">
-//         <h3 className="text-3xl sm:text-5xl 2xl:text-6xl font-bold">
-//           Frontend Inspiration resources from a <br />
-//           <span className="text-[#909090] bg-gradient-to-r from-[#89898C] via-[#69696C] to-[#323235] text-transparent bg-clip-text">Frontend Dev.</span>
-//         </h3>
-//       </div>
-//       <p className="text-xs sm:text-base w-[80%] sm:w-[60%] lg:w-[35%] text-[#7F8080] pt-5">
-//         Discover top-tier tools and resources designed to streamline your
-//         process and elevate your creative and technical skills.
-//       </p>
-//       <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-//     </div>
-//   );
-// };
-
-// export default Hero;
-
-
-
-
-
-
-
 import React from 'react';
 import SearchBar from './SearchBar.jsx';
 
@@ -38,8 +9,9 @@ const isMac = () => {
 const Hero = ({ searchQuery, setSearchQuery, searchInputRef }) => {
   return (
     <div className="w-full mx-auto text-center flex justify-center items-center flex-col py-28 lg:py-32 font-montserrat">
-      <div className="w-[93%] sm:w-[50%] lg:w-[43%] mx-auto">
-        <h3 className="text-3xl sm:text-5xl 2xl:text-6xl font-bold">
+      <div className="w-[90%] sm:w-[70%] mx-auto">
+        {/* <h3 className="text-3xl sm:text-5xl 2xl:text-6xl font-bold"> */}
+        <h3 className="heading-main font-bold">
           Frontend Inspiration resources from a <br />
           <span className="text-[#909090] bg-gradient-to-r from-[#89898C] via-[#69696C] to-[#323235] text-transparent bg-clip-text">Frontend Dev.</span>
         </h3>
