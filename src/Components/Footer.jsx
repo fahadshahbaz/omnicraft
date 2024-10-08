4dfc3458dd92f02f821e9e70eeedaccc15576634
Fahad Shahbaz <fahadshahbaz166@gmail.com>
2024-10-08 23:52:54 +0500

    Minor tweak & new resource add

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,3 +9,39 @@ const Footer = () => {
 };
 
 export default Footer;
+
+// import React from "react";
+// import { linkedin, github, twitter } from "../assets/";
+
+// const Footer = () => {
+//   return (
+//     <footer className="w-full  border-t-2 border-[#2b2b2b] mt-32 ">
+//       <div className="max-w-screen-xl mx-auto flex justify-around items-center p-6">
+//         <div>
+//           <div>logo = </div>
+//           <div>logo = </div>
+//           <div>email = </div>
+//         </div>
+//         <div className="space-y-4">
+//           <h1>About the project</h1>
+//           <p className="w-72">
+//           OmniCraft is your go-to resource for developers and designers. Explore a collection of inspiring websites, assets, and UI library components. Elevate your projects with curated tools and inspiration!
+//           </p>
+//           <div className="flex items-center justify-start gap-6">
+//             <div className="bg-gray-800 p-2 rounded-sm">
+//             <img src={linkedin} alt="linkedin" className="size-6 filter invert"/>
+//             </div>
+//             <div className="bg-gray-800 p-2 rounded-sm">
+//             <img src={github} alt="linkedin" className="size-6 filter invert"/>
+//             </div>
+//             <div className="bg-gray-800 p-2 rounded-sm">
+//             <img src={twitter} alt="linkedin" className="size-6 filter invert"/>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+export { default as github } from "./github.png";
+export { default as linkedin } from "./linkedin.png";
+export { default as twitter } from "./twitter.png";
