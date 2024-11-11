0ad55f92928d3cc539ba16d5ceba8dc03856da13
Fahad Shahbaz <fahadshahbaz166@gmail.com>
2024-11-11 22:51:32 +0500

    update footer design

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,42 +1,35 @@
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { TbBrandGithubFilled } from "react-icons/tb";
+
 const Footer = () => {
   return (
-    <footer className="mt-20 p-5 font-roboto bg-[#1C1C1C]">
-      <h3 className="text-center font-medium text-base">
-        Made with 🤍 by <a href="https://github.com/fahadshahbaz"> Fahad.</a>
-      </h3>
+    <footer className="mt-20 font-roboto space-y-6 pb-3">
+      <div className="w-full h-[1px] bg-[#212223]" />
+      <div className="w-full mx-auto flex justify-around items-center py-1">
+        <p className="text-center font-medium text-base text-zinc-300">
+          © 2023 OmniCraft
+          {/* <a href="https://fahadshahbaz.vercel.app" target="_blank">
+            Fahad.
+          </a> */}
+        </p>
+        <div className="flex items-center space-x-4 justify-center">
+          <p className="hidden sm:block text-neutral-600 mr-2 text-sm">
+            fahadshahbaz166@gmail.com
+          </p>
+          <a href="https://www.linkedin.com/in/fahadshahbaz/" target="_blank">
+            <FaLinkedinIn />
+          </a>
+          <a href="https:/x.com/fahadshahbaz_" target="_blank">
+            <FaXTwitter />
+          </a>
+          <a href="https://github.com/fahadshahbaz" target="_blank">
+            <TbBrandGithubFilled />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
 
 export default Footer;
-
-// import { FaLinkedinIn } from "react-icons/fa";
-// import { FaXTwitter } from "react-icons/fa6";
-// import { TbBrandGithubFilled } from "react-icons/tb";
-
-// const Footer = () => {
-//   return (
-//     <footer className="w-full mt-32 p-6">
-//       <div className="max-w-screen-xl space-y-16 mx-auto p-12 px-24 bg-neutral-900 rounded-lg">
-//         {/* Upper part of footer */}
-//         <div className="flex justify-between items-center">
-//           <h2 className="text-5xl">Design. Create. Elevate.</h2>
-//           <button className="border border-white px-4 py-2 rounded-lg">Contact</button>
-//         </div>
-//         {/* Lower part of footer */}
-//         <div className="w-full h-[2px] bg-zinc-800"></div>
-//         <div className="flex justify-between items-center space-y-4">
-//           <p className="text-2xl">Omnicraft - <span className="text-zinc-500 text-lg">Made by Fahad.</span></p>
-//           <div className="flex items-center space-x-4 justify-center">
-//             <p className="text-zinc-500">fahadshahbaz166@gmail.com</p>
-//           <FaLinkedinIn />
-//           <FaXTwitter />
-//           <TbBrandGithubFilled />
-//           </div>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
