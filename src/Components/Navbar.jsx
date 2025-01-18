9ca002e05e355cf1e1cf0498db4a1d247698ac0d
Fahad Shahbaz <fahadshahbaz166@gmail.com>
2025-01-18 15:58:22 +0500

    Navbar button fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import box from "../assets/icons/box.svg"
+import box from "../assets/icons/box.svg";
 
 const Navbar = () => {
   return (
@@ -6,31 +6,32 @@ const Navbar = () => {
       <nav className="flex justify-between items-center p-4 px-4 sm:px-16">
         <div className="flex items-center justify-center space-x-3">
           <img src={box} alt="logo" className="size-9 inline-block" />
-        <a
-          href="#home"
-          className="text-3xl sm:text-4xl font-bold cursor-pointer"
+          <a
+            href="#home"
+            className="text-3xl sm:text-4xl font-bold cursor-pointer"
           >
-          OmniCraft<span className="text-4xl text-gray-500">.</span>
-        </a>
-          </div>
+            OmniCraft<span className="text-4xl text-gray-500">.</span>
+          </a>
+        </div>
         <a
-          href="https://github.com/fahadshahbaz"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-black bg-white font-medium rounded-lg text-base px-3 pl-4 sm:px-5 py-2.5 text-center inline-flex items-center me-2 hover:scale-105 transition-all ease-linear duration-100"
-          aria-label="Visit my GitHub profile"  // Add aria-label here
-        >
+        href="https://github.com/fahadshahbaz"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Visit my GitHub profile"  // Add aria-label here
+        className="flex justify-center items-center bg-white text-black text-sm sm:text-regular px-5 py-2 rounded-md font-semibold hover:bg-gray-300 duration-300 transition-all linear cursor-pointer">
           <svg
-            className="w-4 h-4"
-            aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
+            viewBox="0 0 24 24"
+            width="1.5em"
+            height="1.5em"
           >
-            <path d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" />
+            <path
+              fill="currentColor"
+              fillRule="evenodd"
+              d="M12.006 2a9.85 9.85 0 0 0-6.484 2.44a10.32 10.32 0 0 0-3.393 6.17a10.48 10.48 0 0 0 1.317 6.955a10.05 10.05 0 0 0 5.4 4.418c.504.095.683-.223.683-.494c0-.245-.01-1.052-.014-1.908c-2.78.62-3.366-1.21-3.366-1.21a2.7 2.7 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621c.317.044.62.163.885.346c.266.183.487.426.647.71c.135.253.318.476.538.655a2.08 2.08 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37c-2.219-.259-4.554-1.138-4.554-5.07a4.02 4.02 0 0 1 1.031-2.75a3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05c.37.858.406 1.828.101 2.713a4.02 4.02 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.47 2.47 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814c0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421a10.47 10.47 0 0 0 1.313-6.948a10.32 10.32 0 0 0-3.39-6.165A9.85 9.85 0 0 0 12.007 2Z"
+              clipRule="evenodd"
+            ></path>
           </svg>
-          {/* Visually Hidden Text */}
-          <span className="sr-only">Visit my GitHub profile</span>  
         </a>
       </nav>
       <div className="flex justify-center items-center">
