5ec66e2445ab535067d10153d47c699a93c4b5c9
Fahad Shahbaz <fahadshahbaz166@gmail.com>
2025-01-19 17:09:08 +0500

    Minor changes related search bar

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,41 +1,12 @@
-// const SearchBar = ({ searchQuery, setSearchQuery, searchInputRef }) => {
-//   return (
-//     <>
-//       <div className="flex justify-center bg-[#181818] rounded-xl mt-8 border border-[#2b2b2b]">
-//         <form onSubmit={(e) => setSearchQuery(e.target.search.value)}>
-//           <input
-//             type="text"
-//             placeholder="Search for resources"
-//             value={searchQuery}
-//             // onChange={(e) => setSearchQuery(e.target.value)}
-//             className="w-full border-none bg-transparent px-2 sm:px-6 placeholder:text-zinc-700 text-white outline-none focus:outline-none"
-//             name="search"
-//             ref={searchInputRef} // Attach the ref to the input element
-//           />
-//           <button
-//             type="submit"
-//             className="m-2 rounded text-black font-medium bg-[#FF3D00] px-3 sm:px-5 py-[0.4rem]"
-//           >
-//             Search
-//           </button>
-//         </form>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default SearchBar;
-
-
 import { useState } from 'react';
 
-const SearchBar = ({ searchQuery, setSearchQuery, searchInputRef }) => {
+const SearchBar = ({ setSearchQuery, searchInputRef }) => {
   const [inputValue, setInputValue] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setSearchQuery(inputValue);
-    setInputValue(''); // Clear the input after search
+    setInputValue(''); 
   };
 
   return (
--- src/Components/SearchModal.jsx
+++ src/Components/SearchModal.jsx
@@ -1,41 +1,3 @@
-// import { IoSearch } from "react-icons/io5";
-
-// const SearchModal = ({ isOpen, searchQuery, setSearchQuery, searchInputRef, onClose }) => {
-//   if (!isOpen) return null;
-
-//   const handleKeyDown = (event) => {
-//     if (event.key === "Enter") {
-//       onClose();
-//     }
-//   };
-
-//   return (
-//     <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-lg bg-black bg-opacity-50">
-//       <div className="w-[90%] sm:w-[400px] bg-[#252525] rounded-md px-4 py-2 shadow-xl flex items-center justify-center">
-//         <IoSearch className="size-6"/>
-//         <input
-//           type="text"
-//           placeholder="Search for resources..."
-//           value={searchQuery}
-//           onChange={(e) => setSearchQuery(e.target.value)}
-//           onKeyDown={handleKeyDown} // Listen for Enter key
-//           className="w-full border-none bg-transparent px-4 py-2 placeholder:text-zinc-500 text-white outline-none focus:outline-1"
-//           ref={searchInputRef}
-//         />
-//         <button
-//           onClick={onClose}
-//           className="absolute top-5 right-6 bg-white/65 px-4 py-[0.6rem] rounded-full text-gray-900"
-//         >
-//           ✕
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SearchModal;
-
-
 import { useState } from 'react';
 import { IoSearch } from "react-icons/io5";
 import { RxCross1 } from 'react-icons/rx';
@@ -68,7 +30,7 @@ const SearchModal = ({ isOpen, setSearchQuery, searchInputRef, onClose }) => {
         </form>
         <button
           onClick={onClose}
-          className="absolute top-5 right-6 bg-white/65 px-3 py-[0.5rem] rounded-sm text-gray-900"
+          className="absolute top-5 right-6 bg-white px-3 py-[0.7rem] rounded-full text-gray-900"
         >
           <RxCross1 className='size-5'/>
         </button>
