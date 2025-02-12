331b5c6cdea83c8409a98e07785d1b6b7648b6f5
Fahad Shahbaz <fahadshahbaz166@gmail.com>
2025-02-12 22:23:01 +0500

    Build issue fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,12 @@
 "use client";
 import { useState, useEffect, useRef, useCallback } from "react";
-import Hero from "@/components/Hero.jsx";
-import CardContainer from "@/components/CardContainer";
-import Card from "@/components/Card.jsx";
-import { data } from "@/data/index.js";
-import LoadMoreLessButtons from "@/components/LoadMoreLessButton.jsx";
-import SearchModal from "@/components/SearchModal.jsx";
-import TagsFilter from "@/components/TagsFilter.jsx";
+import Hero from "../components/Hero";
+import CardContainer from "../components/CardContainer";
+import Card from "../components/Card";
+import { data } from "../data/index";
+import LoadMoreLessButtons from "../components/LoadMoreLessButton";
+import SearchModal from "../components/SearchModal";
+import TagsFilter from "../components/TagsFilter";
 
 // Define a constant for the initial number of cards to display
 const INITIAL_CARDS = 8;
