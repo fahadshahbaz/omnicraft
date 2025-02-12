8cac33b5d30b5e7d9ca0f0bc1ce6fb3df41a9e66
Fahad Shahbaz <fahadshahbaz166@gmail.com>
2025-02-12 23:44:38 +0500

    Fix: Update output directory for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Enables static export mode
+  output: "export", // This enables static export mode
   images: {
     remotePatterns: [
       {
