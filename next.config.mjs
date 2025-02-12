c84d6853929f91f9e65c74a11dc51451da3b6329
Fahad Shahbaz <fahadshahbaz166@gmail.com>
2025-02-12 23:36:25 +0500

    Fixed deployment issues & optimized for static hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,12 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // Enables static export mode
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "i.postimg.cc",
-        pathname: "/**", // Allows all paths on postimg.cc
+        pathname: "/**",
       },
     ],
   },
