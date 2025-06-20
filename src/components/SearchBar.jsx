4dcab7a86550517bbc28168e4df3e916defd2675
Fahad Shahbaz <fahadgujjar166@gmail.com>
2025-06-20 18:31:30 +0500

    fix: clean up Search Bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -18,21 +18,13 @@ export default function SearchBar({ setSearchQuery, searchInputRef }) {
           placeholder="Search for resources"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
-          className="flex-1 border-none bg-transparent px-4 sm:px-6 placeholder:text-zinc-700 
-          text-white outline-none focus:outline-none"
+          className="flex-1 border-none bg-transparent px-4 sm:px-6 placeholder:text-zinc-700 text-white outline-none focus:outline-none"
           name="search"
           ref={searchInputRef}
         />
-        {/* <button
-          type="submit"
-          className="m-2 rounded text-black font-medium bg-[#FF3D00] px-3 sm:px-5 py-[0.4rem]"
-        >
-          Search
-        </button> */}
         <button
           type="submit"
-          className="m-2 rounded-full text-black font-medium bg-[#FF3D00] px-4 sm:px-9 py-[0.6rem]
-           sm:py-[0.8rem] shadow-[0_8px_20px_4px_rgba(255,58,0,0.3)]"
+          className="m-2 rounded-full text-black font-medium bg-[#FF3D00] px-4 sm:px-9 py-[0.6rem] sm:py-[0.8rem] shadow-[0_8px_20px_4px_rgba(255,58,0,0.3)]"
         >
           Search
         </button>
