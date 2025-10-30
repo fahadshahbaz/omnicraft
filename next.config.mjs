142f536b7e03bab392c4dc6e11770e53a9b35328
Fahad Shahbaz <fahadgujjar166@gmail.com>
2025-10-30 22:51:23 +0500

    feat: change image hosting to cloudinary

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'i.postimg.cc',
-          pathname: '/**',
-        },
-      ],
-    },
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-  export default nextConfig;
+export default nextConfig;
