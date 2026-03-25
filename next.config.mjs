9afd313192ac1524fcccd988c7ac4934f29204ef
Fahad Shahbaz <fahadgujjar166@gmail.com>
2026-03-25 15:58:01 +0500

    feat: configure Next.js to allow SVG images and new remote domains, and add `baseline-browser-mapping` dev dependency.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "v1.screenshot.11ty.dev",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.microlink.io",
         pathname: "/**",
       },
       {
