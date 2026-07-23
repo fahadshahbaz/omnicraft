c3c361afa46cf92f3ddae79fa7f5ab0dd98845c8
Fahad Shahbaz <fahadgujjar166@gmail.com>
2026-07-23 20:26:34 +0500

    fix: correct padding in CardContainer component

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -8,7 +8,7 @@ interface CardContainerProps {
 
 export default function CardContainer({ children }: CardContainerProps) {
 	return (
-		<div className="container-card w-11/12 max-w-screen-2xl flex flex-wrap justify-center pt-8 gap-5 mx-auto overflow-hidden">
+		<div className="container-card w-11/12 max-w-screen-2xl flex flex-wrap justify-center py-8 gap-5 mx-auto overflow-hidden">
 			<AnimatePresence mode="popLayout">{children}</AnimatePresence>
 		</div>
 	);
